35382f43bc54fe3068bd6a99dc329d53061e9666
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 23:04:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ from os import environ
 API_ID = int(environ.get("API_ID", "14091414"))
 API_HASH = environ.get("API_HASH", "1e26ebacf23466ed6144d29496aa5d5b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo")
-ADMIN = int(environ.get("ADMIN", "5500572462, 5205602399"))          
+ADMIN = [int(admin_id) for admin_id in environ.get("ADMIN", "5500572462,5205602399").split(",")]
 CAPTION = environ.get("CAPTION", "loda")
 
-# for thumbnail ( back end is MrMKN brain 😉)
+# for thumbnail (back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
-
