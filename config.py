ee51cfbed34046bc7bc204957b872755e87fc17c
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-06-16 23:03:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "14091414"))
 API_HASH = environ.get("API_HASH", "1e26ebacf23466ed6144d29496aa5d5b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "5752952621:AAGO61IiffzN23YuXyv71fbDztA_ubGM6qo")
-ADMIN = int(environ.get("ADMIN", "5500572462"))          
+ADMIN = int(environ.get("ADMIN", "5500572462 5205602399"))          
 CAPTION = environ.get("CAPTION", "loda")
 
 # for thumbnail ( back end is MrMKN brain 😉)
